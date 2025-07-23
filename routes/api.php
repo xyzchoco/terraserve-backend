692ddf066c3d63bb67a8f5828a2b8fb3e93ee5a7
morea <m.alvindzakyananta@gmail.com>
2025-07-24 00:38:39 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\ProductCategoryController;
+
+Route::get('products', [ProductController::class, 'all']);
+Route::get('categories', [ProductCategoryController::class, 'all']);
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+    
+    Route::get('transaction',[ TransactionController::class, 'all']);
+    Route::post('checkout',[ TransactionController::class, 'checkout']);
+});
